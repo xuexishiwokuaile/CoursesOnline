2768a1366d58e6e8fa3c37831e6e61d1d9eed035
xuexishiwokuaile <37232631+xuexishiwokuaile@users.noreply.github.com>
2020-03-07 14:22:51 +0800

    Add files via upload

---
+++ FrontEnd/utils/util.js
@@ -0,0 +1,46 @@
+function formatTime(date) {
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
+
+  var hour = date.getHours()
+  var minute = date.getMinutes()
+  var second = date.getSeconds()
+
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+function formatTime2(date) {
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
+
+  var hour = date.getHours()
+  var minute = date.getMinutes()
+  var second = date.getSeconds()
+
+
+  return [month, day].map(formatNumber).join('')
+}
+function formatTime3(date) {
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
+
+  var hour = date.getHours()
+  var minute = date.getMinutes()
+  var second = date.getSeconds()
+
+
+  return [hour, minute,second].map(formatNumber).join('')
+}
+function formatNumber(n) {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+module.exports = {
+  formatTime: formatTime2,
+  formatTime2: formatTime3
+}
